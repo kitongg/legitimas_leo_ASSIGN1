2631beac74a96bdfc41d95cc9ade714c80968a96
= <leolegitimas.official@gmail.com>
2025-10-08 14:24:58 +0800

    editing the home and about

--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, HttpResponse
+
+
+def base(request):
+   return render(request, "base.html")
+
+def home(request):
+   content = {
+      'title':  'Home',
+      'course': 'MSIT210 - DevOps',
+   }
+   return render(request, "home.html", content)
+
+def about(request):
+
+   studentInfo = {
+      'studentName': 'Leo Niño Y. Legitimas',
+      'studentNum': '2025-41000',
+   }
+   return render(request, "about.html", studentInfo)
